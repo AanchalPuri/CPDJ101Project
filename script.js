4b9a7324cb9356974c080fcdf2ea6f3fcdb43ed3
Aanchal Puri <puriaanchal7@gmail.com>
2026-04-26 21:07:24 -0400

    Added the welcome page that has the survey.
    The welcome page displays a QR code and link for the audience to take in
    order to gain some understanding of the common misconceptions of
    deepfakes. Right now, everything is a placeholder for when we actually
    have the survey created. People with the password are allowed to see the
    results of the survey. Continue button takes the user to the rest of the
    website.

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
 function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
+  var i, tabcontent, tablinks;
   
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].classList.remove("active");
-    }
-  
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.classList.add("active");
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+  
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.classList.add("active");
+}
 
-  document.querySelector(".tablinks").click();
+function enterSite() {
+  document.getElementById('splash').style.display = 'none';
+  document.getElementById('main-site').style.display = 'block';
+  document.body.style.overflow = 'auto';
+  document.querySelector('.tablinks').click();
+}
+
+function checkPassword() {
+  const input = document.getElementById('results-password').value;
+  const msg = document.getElementById('password-msg');
+  const correctPassword = 'cpdj101'; // change this to whatever you want
+
+  if (input === correctPassword) {
+    msg.textContent = '✓ Correct! Redirecting...';
+    msg.className = 'password-msg success';
+    setTimeout(() => {
+      window.open('https://docs.google.com/forms/placeholder-results-link', '_blank');
+    }, 1000);
+  } else {
+    msg.textContent = '✗ Incorrect password.';
+    msg.className = 'password-msg error';
+    document.getElementById('results-password').value = '';
+  }
+}
+
+document.querySelector(".tablinks").click();
+
